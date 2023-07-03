8fed719fad335aeba6b6213fa5856aa8c04d560e
Amogh <amoghshakya30@gmail.com>
2023-07-03 22:44:23 +0545

    create category service and refactor code

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -4,39 +4,31 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AddCategoryRequest;
-use App\Models\Category;
-use App\Models\Subcategory;
+use App\Models\{Category, Subcategory};
+use App\Services\CategoryService;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\{JsonResponse, RedirectResponse};
 use Intervention\Image\Facades\Image;
 
 class CategoryController extends Controller
 {
     protected Category $categoryModel;
     protected Subcategory $subcategoryModel;
+    protected CategoryService $categoryService;
 
-    public function __construct(Category $categoryModel, Subcategory $subcategoryModel)
+    public function __construct(Category $categoryModel, Subcategory $subcategoryModel, CategoryService $categoryService)
     {
         $this->categoryModel = $categoryModel;
         $this->subcategoryModel = $subcategoryModel;
+        $this->categoryService = $categoryService;
     }
 
-    public function allCategory()
+
+    public function allCategory(): JsonResponse
     {
         $categories = $this->categoryModel->select(['category_name', 'category_image'])->get();
-        $categoryDetailsArray = [];
-        foreach ($categories as $category) {
-            $Sub_category = $this->subcategoryModel->category($category->category_name)->get();
-            $item = [
-                'category_name' => $category->category_name,
-                'category_image' => $category->category_image,
-                'subcategory_name' => $Sub_category,
-            ];
-
-            array_push($categoryDetailsArray, $item);
-        }
-
-        return $this->successResponse(['category' => $categoryDetailsArray], "Successfully Retrieved");
+        $category = $this->categoryService->allCategory($categories);
+        return $this->successResponse(['category' => $category], "Successfully Retrieved");
     }
 
 
@@ -53,22 +45,32 @@ class CategoryController extends Controller
     }
 
 
+    // public function storeCategory(AddCategoryRequest $request): RedirectResponse
+    // {
+    //     $category_name = $request->category_name;
+    //     $file = $request->file('category_image');
+    //     if ($file) {
+    //         $image_name = date('YmdHi') . $file->getClientOriginalName();
+    //         Image::make($file)->resize(128, 128)
+    //             ->save(public_path('storage/images/') . $image_name);
+    //     }
+    //     $image_url = "http://localhost:8000/storage/images/" . $image_name;
+    //     $Category_create = $this->categoryModel->create([
+    //         'category_name' => $category_name,
+    //         'category_image' => $image_url,
+    //     ]);
+
+    //     $notification = $this->notification($Category_create, 'Category Succesfully Created', 'Failed To Create Category');
+    //     return  redirect(route("category.list"))->with('notification', $notification);
+    // }
+
+
     public function storeCategory(AddCategoryRequest $request): RedirectResponse
     {
-        $category_name = $request->category_name;
+        $validated = $request->validated();
         $file = $request->file('category_image');
-        if ($file) {
-            $image_name = date('YmdHi') . $file->getClientOriginalName();
-            Image::make($file)->resize(128, 128)
-                ->save(public_path('storage/images/') . $image_name);
-        }
-        $image_url = "http://localhost:8000/storage/images/" . $image_name;
-        $Category_create = $this->categoryModel->create([
-            'category_name' => $category_name,
-            'category_image' => $image_url,
-        ]);
-
-        $notification = $this->notification($Category_create, 'Category Succesfully Created', 'Failed To Create Category');
+        $category = $this->categoryService->store($file, $validated);
+        $notification = $this->notification($category, 'Category Succesfully Created', 'Failed To Create Category');
         return  redirect(route("category.list"))->with('notification', $notification);
     }
 
---
+++ app/Services/CategoryService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Resources\SubCategoryResource;
+use App\Models\{Category, Subcategory};
+use Intervention\Image\Facades\Image;
+
+
+class CategoryService
+{
+    protected Category $categoryModel;
+    protected Subcategory $subcategoryModel;
+
+    public function __construct(Category $categoryModel, Subcategory $subcategoryModel)
+    {
+        $this->categoryModel = $categoryModel;
+        $this->subcategoryModel = $subcategoryModel;
+    }
+
+
+    public function allCategory(object $categories): array
+    {
+        $categoryDetails = $categories->map(function ($category) {
+            $sub_category = $this->subcategoryModel->category($category->category_name)->get();
+            return [
+                'category_name' => $category->category_name,
+                'category_image' => $category->category_image,
+                'subcategory_name' => SubCategoryResource::collection($sub_category),
+            ];
+        });
+
+        return $categoryDetails;
+    }
+
+
+    public function saveCategoryImage($file): string
+    {
+        $image_name = date('YmdHi') . $file->getClientOriginalName();
+        Image::make($file)->resize(128, 128)
+            ->save(public_path('storage/images/') . $image_name);
+        return $image_name;
+    }
+
+
+    public function store($file, array $validated): object
+    {
+        if ($file)
+            $image_name = $this->saveCategoryImage($file);
+
+        $image_url = "http://localhost:8000/storage/images/" . $image_name;
+        $validated['category_image'] = $image_url;
+        $category = $this->categoryModel->create($validated);
+        return $category;
+    }
+}
